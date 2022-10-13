a1d1188b426445e175a5fc5a9ab69e35fc05bee8
Igor <igorvlasenko97@gmail.com>
2022-10-14 02:53:40 +0300

    v2.pytinhulo

--- Lab_02/Lab_02.cpp
+++ Lab_02/Lab_02.cpp
@@ -9,7 +9,7 @@ int main()
 	double z1; // результат обчислення 1-го виразу
 	double z2; // результат обчислення 2-го виразу
 
-	cout << "m = "; cin >> x;
+	cout << "m = "; cin >> x; // test
 
 	z1 = (sin(2 * x) + sin(5 * x) - sin(3 * x)) / (cos(x) + 1 - 2 * (sin(2 * x) * sin(2 * x)));
 	z2 = 2 * sin(x);
